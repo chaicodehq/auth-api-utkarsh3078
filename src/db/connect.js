f2ccb48a67eb35035e07c4db4b3c4e7b7eb6f140
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2026-04-19 18:34:23 +0530

    Step 2 done

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 /**
  * TODO: Connect to MongoDB
@@ -9,4 +9,14 @@ import mongoose from 'mongoose';
  */
 export async function connectDB(uri) {
   // Your code here
+  if (!uri) {
+    throw new Error("MongoDB URI is required");
+  }
+  try {
+    await mongoose.connect(uri);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+  return mongoose.connection;
 }
